d83a37f9dfd58ba37ed2dba05f462ec86775cbca
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-07-26 23:38:18 +0530

    Designed Login Page

---
+++ frontend/src/components/login/Login.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react"; 
+import useLogin from "../../hooks/useLogin";
+
+
+function Login() {
+const[username,setUsername]=useState("")
+const[password,setPassword]=useState("")
+const{loading,login}=useLogin();
+const handleSubmit=async(e)=>{
+e.preventDefault();
+await login(username,password)
+}
+
+  return (
+    <div className="bg-hero h-screen no-repeat bg-center bg-cover w-screen flex flex-col justify-center items-center p-4">
+      <h1 className="text-3xl font-bold text-black mb-8 font-serif">Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div className="rounded-md border border-gray-500 justify-center items-center p-5 mb-20 backdrop-blur bg-orange-300">
+          <label className="label p-2">
+            <span className="text-base label-text ">Username</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter username"
+            className="w-full input input-bordered h-10"
+            value={username}
+            onChange={(e)=>setUsername(e.target.value)}
+          />
+
+          <label className="label p-2">
+            <span className="text-base label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            className="w-full input input-bordered h-10"
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
+          />
+
+          <p className="m-2 font-serif">Don't have an account? Register here </p>
+         
+          <button className='btn btn-block btn-sm mt-2'>
+							Login
+						</button>
+        </div>
+      </form>
+
+      
+    </div>
+  );
+}
+
+export default Login;
