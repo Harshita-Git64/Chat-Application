7c6c9500d94a32a28a044a72c948ce797ed44c2e
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-08-02 23:51:20 +0530

    Create custom hooks

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import toast from 'react-hot-toast'
+import { useAuthContext } from '../context/AuthContext';
+
+function useLogin() {
+
+const [loading,setLoading]=useState(false)
+const { setAuthUser } = useAuthContext();
+
+const login=async(username,password)=>{
+const isValid=handleValidations(username,password)
+if(!isValid) return;
+console.log(username,password)
+setLoading(true)
+try {
+    const res = await fetch("http://localhost:5000/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password })
+    });
+    const data = await res.json();
+    if (data.error) {
+        throw new Error(data.error);
+    }
+    localStorage.setItem("chat-user", JSON.stringify(data));
+    setAuthUser(data);
+    
+} catch (error) {
+    toast.error(error.message);
+} finally {
+    setLoading(false);
+}
+}
+ return {loading,login}
+}
+function handleValidations(username,password){
+   
+    if(!username || !password ){
+        toast.error("fields can't be empty")
+        return false;
+    }
+    if(password.length<6){
+        toast.error("Password should not be less than 6 characters")
+        return false;
+    }
+   return true;
+}
+export default useLogin;
---
+++ frontend/src/hooks/useSendMessage.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import toast from 'react-hot-toast'
+import useConversation from '../zustand/useConversation';
+
+function useSendMessage() {
+ const[loading,setLoading]=useState(false);
+ const{messages,setMessages,selectedConversation}=useConversation()
+
+ const sendMessage=async(textmsg)=>{
+   setLoading(true)
+ try{
+   
+   const res = await fetch(`http://localhost:5000/api/message/send/${selectedConversation._id}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({textmsg}),
+      credentials: "include"
+  });
+  const data = await res.json();
+  console.log("message is",data)
+  if (data.error) {
+      throw new Error(data.error);
+  }
+    await setMessages([...messages,data])
+ }
+ catch (error) {
+   toast.error(error.message);
+} finally {
+   setLoading(false);
+}
+ }
+ return {loading,sendMessage}
+
+}
+
+export default useSendMessage
