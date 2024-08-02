4e516821e6317606255d72db6e6a95498d267790
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-08-02 23:53:54 +0530

    Create useContext for state management

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuthContext = () => {//hook
+	return useContext(AuthContext);//useContext accept object/value returned by createContext() and it return current context value.
+};
+
+export const AuthContextProvider = ({ children }) => { // component
+	const [authUser, setAuthUser] = useState(JSON.parse(localStorage.getItem("chat-user")) || null);
+
+	return <AuthContext.Provider value={{ authUser, setAuthUser }}>{children}</AuthContext.Provider>;
+};
