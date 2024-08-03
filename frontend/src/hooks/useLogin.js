d6196cf0d4cc5978f7a89f00489fc9bcf64c04d8
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-08-04 03:17:31 +0530

    fetch server routes

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -16,7 +16,8 @@ try {
     const res = await fetch("http://localhost:5000/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify({ username, password }),
+        credentials: "include", 
     });
     const data = await res.json();
     if (data.error) {
--- frontend/src/hooks/useSendMessage.js
+++ frontend/src/hooks/useSendMessage.js
@@ -6,18 +6,17 @@ function useSendMessage() {
  const[loading,setLoading]=useState(false);
  const{messages,setMessages,selectedConversation}=useConversation()
 
- const sendMessage=async(textmsg)=>{
+ const sendMessage=async(message)=>{
    setLoading(true)
  try{
-   
    const res = await fetch(`http://localhost:5000/api/message/send/${selectedConversation._id}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({textmsg}),
+      body: JSON.stringify({message}),
       credentials: "include"
   });
   const data = await res.json();
-  console.log("message is",data)
+ // console.log("data",data)
   if (data.error) {
       throw new Error(data.error);
   }
