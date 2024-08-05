dfcbb8ab2f14da92b8767700c19e561bb0079be3
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-08-06 01:33:25 +0530

    fixed minor changes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,96 +1,85 @@
 import User from "../models/user.model.js";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 import jsonWebTokenMethod from "../utils/generateToken.js";
 
-
-export const signup=async(req,res)=>{
-    try{
-    const {fullName,username,password,confirmPassword,gender}=req.body;
-    if(password!=confirmPassword){
-        return res.status(404).json({error:"Passwords do not match"})
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+    if (password != confirmPassword) {
+      return res.status(404).json({ error: "Passwords do not match" });
     }
-    const user= await User.findOne({username})
-    if(user)
-    {
-        return res.status(400).json({error:"User already exists"})
+    const user = await User.findOne({ username });
+    if (user) {
+      return res.status(400).json({ error: "User already exists" });
     }
 
-    const boyProfilePic=`https://avatar.iran.liara.run/public/boy?username=${username}`
-    const girlProfilePic=`https://avatar.iran.liara.run/public/girl?username=${username}`
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword= await bcrypt.hash(password,salt);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-    const newUser=new User({
-        fullName,
-        username,
-        password:hashedPassword,
-        gender,
-        profilePic:gender==="male"?boyProfilePic:girlProfilePic
-    })
-    if(newUser){
-        jsonWebTokenMethod(newUser._id,res)
-        await newUser.save();
-    
-        res.status(201).json({
-            _id:newUser._id,
-            fullName:newUser.fullName,
-            username:newUser.username,
-            profilePic:newUser.profilePic
-        })
-    }
-    else{
-        return res.status(400).json({error:"Invalid user data"})
-    }
-}
-catch(error){
-console.log("error in signup",error.message)
-res.status(500).json({error:"internal server error found"})
-}
-}
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+    if (newUser) {
+      jsonWebTokenMethod(newUser._id, res);
+      await newUser.save();
 
-export const login=async(req,res)=>{
-    try{
-        const{username,password}=req.body;
-        const user=await User.findOne({username})
-        const isPassword=await bcrypt.compare(password,user?.password || "")
-        if(!user || !isPassword)
-        {
-           return res.status(400).json({error:"invalid user credentials"})
-        }
-        jsonWebTokenMethod(user._id,res)
-           // res.status(200).json({message:"user logged in sucessfully"})
-            res.status(201).json({
-                _id:user._id,
-                fullName:user.fullName,
-                username:user.username,
-                profilePic:user.profilePic
-            })
-        
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      return res.status(400).json({ error: "Invalid user data" });
     }
-    catch(error){
-        console.log("Error in login:",error.message)
-        res.status(500).json({error:"internal sever error"})
-    }
-}
+  } catch (error) {
+    console.log("error in signup", error.message);
+    res.status(500).json({ error: "internal server error found" });
+  }
+};
 
-export const logout=async(req,res)=>{
-    try{
-    res.cookie("cookies","",{maxAge:0})
-   return res.status(200).json({message:"user logged out sucessfully"})
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPassword = await bcrypt.compare(password, user?.password || "");
+    if (!user || !isPassword) {
+      return res.status(400).json({ error: "invalid user credentials" });
     }
-    catch(error){
-        console.log("error in logout",error.message)
-        res.status(501).json({message:"Internal server error"})
-    }
-}
-export const getUserDetails=async(req,res)=>{
-    try{
-        const Users=await User.find({})
-        res.json(Users)
-    }
-    catch(errors)
-    {
-        console.log("error is",errors.message)
-    }
-}
+    jsonWebTokenMethod(user._id, res);
+    res.status(201).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login:", error.message);
+    res.status(500).json({ error: "internal sever error" });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.cookie("cookies", "", { maxAge: 0 });
+    return res.status(200).json({ message: "user logged out sucessfully" });
+  } catch (error) {
+    console.log("error in logout", error.message);
+    res.status(501).json({ message: "Internal server error" });
+  }
+};
+export const getUserDetails = async (req, res) => {
+  try {
+    const Users = await User.find({});
+    res.json(Users);
+  } catch (errors) {
+    console.log("error is", errors.message);
+  }
+};
--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,67 +1,66 @@
 import Message from "../models/message.model.js";
 import Conversation from "../models/conversation.model.js";
+import { getReceiverSocketId, io } from "../Socket/socket.js";
 
-export const sendMessage=async(req,res)=>{
-    try{
+export const sendMessage = async (req, res) => {
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
 
-        const { message } = req.body;
-		const { id: receiverId } = req.params;
-		const senderId = req.user._id;
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
 
-		let conversation = await Conversation.findOne({
-			participants: { $all: [senderId, receiverId] },
-		});
-
-		if (!conversation) {
-			conversation = await Conversation.create({
-				participants: [senderId, receiverId],
-			});
-		}
-
-		const newMessage = new Message({
-			senderId,
-			receiverId,
-			message,
-		});
-
-		if (newMessage) {
-			conversation.messages.push(newMessage._id);
-		}
-
-        await conversation.save();
-		 await newMessage.save();
-
-		// this will run in parallel
-		//await Promise.all([conversation.save(), newMessage.save()]);
-
-        res.status(200).json(newMessage)
-
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
     }
-    catch(error){
-        res.status(500).json({error:"internal server error"})
-        console.log("error on sending message",error.message)
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
     }
-}
+    await conversation.save();
+    await newMessage.save();
 
-export const getMessage=async(req,res)=>{
-	try{
-		const { id: receiverId } = req.params;
-		const senderId = req.user._id;
+    // this will run in parallel
+    //await Promise.all([conversation.save(), newMessage.save()]);
+    // SOCKET IO FUNCTIONALITY WILL GO HERE
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      // io.to(<socket_id>).emit() used to send events to specific client
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+    res.status(200).json(newMessage);
+  } catch (error) {
+    res.status(500).json({ error: "internal server error" });
+    console.log("error on sending message", error.message);
+  }
+};
 
-		let conversation = await Conversation.findOne({
-			participants: { $all: [senderId, receiverId] },
-		}).populate("messages");//not reference but actual message info from messagemmodel.
-        
-		if (!conversation) {
-		return res.status(200).json([])
-		}
-		const messages=conversation.messages;
-		return res.status(200).json(messages)
+export const getMessage = async (req, res) => {
+  try {
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
 
-	}
-	catch(error){
-        console.log("error on getting message",error.message)
-		return res.status(500).json({error:"internal server error"})
-	}
-	
-}
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    }).populate("messages"); //not reference but actual message info from messagemmodel.
+
+    if (!conversation) {
+      return res.status(200).json([]);
+    }
+    const messages = conversation.messages;
+    return res.status(200).json(messages);
+  } catch (error) {
+    console.log("error on getting message", error.message);
+    return res.status(500).json({ error: "internal server error" });
+  }
+};
