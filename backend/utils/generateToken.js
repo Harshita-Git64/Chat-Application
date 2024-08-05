a00c675c7bc80943430d4990547fe48d8ce83ea5
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-08-06 01:44:49 +0530

    generate jwt token

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,15 +1,18 @@
-import  jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
-const jsonWebTokenMethod=(userId,res)=>{
-    const token=jwt.sign({userId},process.env.JWT_SECRET_KEY,{expiresIn:"90d"}) //first argument is payload which embedded in token, it can be multiple values and to verify the token payload is needed,second argu is secret key and third arg determines the expiry time of token.
-    const decoded=jwt.verify(token,process.env.JWT_SECRET_KEY)
-    console.log("token: ",token)//long unique string
-    console.log("decoded: ",decoded)//object containing payload details (passed in jwt.sign) like userId,iat,exp
+const jsonWebTokenMethod = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "90d",
+  }); //first argument is payload which embedded in token, it can be multiple values and to verify the token payload is needed,second argu is secret key and third arg determines the expiry time of token.
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  // console.log("token: ",token)//long unique string
+  // console.log("decoded: ",decoded)//object containing payload details (passed in jwt.sign) like userId,iat,exp
 
-    res.cookie("jwt",token,{
-        maxAge:24*60*60*1000,//format in  milliseconds
-        httpOnly:true,//prevent attacks
-        secure:process.env.NODE_ENV !=="Development"//true in production only
-    })
-}
+  res.cookie("jwt", token, {
+    maxAge: 24 * 60 * 60 * 1000, //format in  milliseconds
+    httpOnly: true, //prevent attacks
+    secure: process.env.NODE_ENV !== "Development", //true in production only
+    sameSite: "strict",
+  });
+};
 export default jsonWebTokenMethod;
