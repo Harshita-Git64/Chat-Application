d5b1fa10b36cc4423f80deacf935daeb4209e039
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-08-06 02:13:21 +0530

    Wrap whole components into context and socket provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import { AuthContextProvider } from './context/AuthContext'
+import { SocketContextProvider } from './context/SocketContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
+    <BrowserRouter>
+    <AuthContextProvider>
+    <SocketContextProvider>
     <App />
-  </React.StrictMode>,
+    </SocketContextProvider>    
+    </AuthContextProvider>
+    </BrowserRouter>
+    
 )
