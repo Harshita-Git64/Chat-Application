7be45f9324353f41b260b6ee0b77c54bb8104306
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-08-06 01:40:51 +0530

    Add timestamps

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -15,7 +15,8 @@ const messageSchema=new mongoose.Schema(
         type:String,
         required:true
        }
-    }
+    },
+    { timestamps: true }
 )
 const Message=mongoose.model("Message",messageSchema)
 export default Message
