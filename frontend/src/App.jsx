c7158648be4bffac1ca3b8e7465fc25c0f5b29e3
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-08-07 12:39:41 +0530

    Added Route to handle 404 error status

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,17 +6,17 @@ import SignUp from './components/signup/SignUp'
 import HomePage from './components/Home/HomePage'
 import { Toaster } from "react-hot-toast"
 import { useAuthContext } from "./context/AuthContext"
+import Error from "./components/Error"
 
 function App() {
-  const [count, setCount] = useState(0)
-  const { authUser } = useAuthContext();
-  
+  const { authUser } = useAuthContext();  
   return (
    <div className=''>
     <Routes>
       <Route path='/' element={authUser ? <HomePage /> : <Navigate to={"/login"} />} />
 				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
 				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
+        <Route path='*' element={<Error/>} />
     </Routes>
    <Toaster/>
    </div>
